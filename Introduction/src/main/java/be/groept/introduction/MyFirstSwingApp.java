9d1d7ab97924788bac4e5b764b01acff249c75a7
thijsmaes <java@ubuntu>
2012-09-10 21:22:40 +0200

    een compleet mislukt project

--- Introduction/src/main/java/be/groept/introduction/MyFirstSwingApp.java
+++ Introduction/src/main/java/be/groept/introduction/MyFirstSwingApp.java
@@ -3,6 +3,13 @@ package be.groept.introduction;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JEditorPane;
+import javax.swing.JOptionPane;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MyFirstSwingApp {
 
@@ -38,5 +45,20 @@ public class MyFirstSwingApp {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.X_AXIS));
+		
+		JButton btnOpen = new JButton("open");
+		btnOpen.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane joptionPane = new JOptionPane();
+				String url = joptionPane.showInputDialog("Enter URL");
+				editorPane.setPage(url);
+				}
+			
+		});
+		frame.getContentPane().add(btnOpen);
+		
+		final JEditorPane editorPane = new JEditorPane();
+		frame.getContentPane().add(editorPane);
 	}
 }
